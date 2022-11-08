20e8a27c2bc71abf3ee8a6fc6423782b586188b8
C. Lewis <ctj.lewis@icloud.com>
2022-11-08 17:23:19 -0600

    v2.2.2

--- src/utils/openBrowser.ts
+++ src/utils/openBrowser.ts
@@ -5,7 +5,7 @@ import { sleep } from "./misc";
 export const openBrowser = async (url: string) => {
   console.groupCollapsed();
   console.log();
-  console.log("Attempting to open browser for auth. Please visit this link if it does not open automatically.");
+  console.log("Attempting to open browser. Please visit this link if it does not open automatically.");
   console.log();
   console.log(url);
   console.log();
