c6b64c74abca335c687ca7a2b51efcf4a4a2573d
C. Lewis <ctj.lewis@icloud.com>
2022-11-10 07:51:41 -0600

    chore: add `fetch` fallbacks

--- src/cache/fetch.ts
+++ src/cache/fetch.ts
@@ -1,16 +1,32 @@
 import nodeFetch from "node-fetch";
 import { checkCache } from "./cacheToken";
 import { Auth0NodeConfig } from "../types";
+import { createDebugLogger } from "debug-logging";
+import { env } from "process";
 
 export const fetch = async (
   config: Auth0NodeConfig,
   ...params: Parameters<typeof nodeFetch>
 ) => {
+  const DEBUG = createDebugLogger(fetch);
+
   const [url, options] = params;
-  const cachedToken = await checkCache(config);
+  let cachedToken = await checkCache(config);
 
   if (!cachedToken) {
-    throw new Error("User does not have existing session.");
+    DEBUG.log("No cached token found.");
+    if (env.AUTH0_TOKEN) {
+      DEBUG.log("Using AUTH0_TOKEN environment variable.");
+      cachedToken = {
+        access_token: env.AUTH0_TOKEN,
+        expires_in: 3600,
+        scope: "openid profile email",
+        token_type: "Bearer",
+      };
+    } else {
+      DEBUG.log("No cached token, no override set. Falling back to default fetch.");
+      return await nodeFetch(url, options);
+    }
   }
 
   const { access_token: accessToken, token_type: tokenType } = cachedToken;
