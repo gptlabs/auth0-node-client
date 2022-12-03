a90b8b42739d4a8271a868b8c8ff7bf3ef7aa22d
C. Lewis <ctj.lewis@icloud.com>
2022-12-03 03:37:36 -0600

    fix: catch failed `open()` calls

--- src/utils/openBrowser.ts
+++ src/utils/openBrowser.ts
@@ -7,7 +7,11 @@ export const openBrowser = async (url: string) => {
   log("Attempting to open browser. Please visit this link if it does not open automatically.");
   log(url, ["underline"]);
 
-  await sleep(500);
-  await open(url);
-  await sleep(500);
+  try {
+    await sleep(500);
+    await open(url);
+    await sleep(500);
+  } catch (e) {
+    log("Unable to open browser automatically. Please visit the link above.");
+  }
 };
